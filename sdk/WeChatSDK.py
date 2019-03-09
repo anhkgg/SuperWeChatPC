06ab36b6a81babdc29bf4cfaf26039614404b39c
anhkgg <anhkgg@163.com>
2019-03-09 17:34:00 +0800

    update py sdk

--- sdk/WeChatSDK.py
+++ sdk/WeChatSDK.py
@@ -2,6 +2,10 @@
 
 import ctypes, os, sys
 #https://blog.csdn.net/zmnbehappy/article/details/78697185
+#https://www.cnblogs.com/gaowengang/p/7919219.html
+
+RECVTEXTMSG_CALLBACK = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_wchar_p, ctypes.c_wchar_p)
+RECVPAYMSG_CALLBACK = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_wchar_p)
 
 class WXSDK():
     def __init__(self):
@@ -42,8 +46,24 @@ class WXSDK():
     def WXSendTextMsg(self, wxid, msg):
         return self.sdk.WXSendTextMsg(self.pid, wxid, msg)
 
-    #def WXSendImageMsg(self, wxid, path):
-    #    return self.sdk.WXSendImageMsg(self.pid, wxid, path)
+    def WXRecvTextMsg(self, funptr):
+        return self.sdk.WXRecvTextMsg(self.pid,  funptr)
+
+    def WXRecvTransferMsg(self, funptr):
+        return self.sdk.WXRecvTransferMsg(self.pid,  funptr)
+
+    def WXRecvPayMsg(self, funptr):
+        return self.sdk.WXRecvPayMsg(self.pid,  funptr)
+
+def RecvTextMsg(pid, wxid, msg):
+    print(pid, wxid, msg)
+    return 0
+
+def RecvPayMsg(pid, wxid, id, msg):
+    print(pid, wxid, id, msg)
+    return 0
+
+g_RecvTextMsg = RECVTEXTMSG_CALLBACK(RecvTextMsg)
 
 if __name__ == "__main__":
     wxsdk = WXSDK()
@@ -54,3 +74,5 @@ if __name__ == "__main__":
     print(wxsdk.WXInitialize())
     input("wait for...")
     print(wxsdk.WXSendTextMsg("wxid_n11111", "This is a python sdk test msg"))
+    wxsdk.WXRecvTextMsg(g_RecvTextMsg)
+    input("wait for recv msg...")
